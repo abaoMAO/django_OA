a00e6bd7b25ff02171bd3128b7f5b567c014ee2f
WillGiab <2410250284@qq.com>
2018-03-02 00:11:40 +0800

    数据表设计

--- apps/scores/models.py
+++ apps/scores/models.py
@@ -1,3 +1,65 @@
 from django.db import models
 
 # Create your models here.
+from django.utils.encoding import smart_str
+
+from users.models import UserProfile
+
+
+class Questionnaire(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    modified_at = models.DateTimeField(auto_now=True, editable=False)
+    is_active = models.BooleanField(default=True)
+
+    judges = models.ManyToManyField(UserProfile, verbose_name="评分人")
+    players = models.ManyToManyField(UserProfile, verbose_name="受评人")
+
+    def __str__(self):
+        return smart_str(self.players.username + "的问卷")
+
+    class Meta:
+        verbose_name = u"问卷"
+        verbose_name_plural = verbose_name
+        ordering = ["-created_at"]
+
+
+class Question(models.Model):
+    question_type = models.CharField(max_length=20, verbose_name="评价指标", null=False)
+    question_content = models.CharField(max_length=200, verbose_name="评价要素", null=False)
+    full_score = models.IntegerField(max_length=100, verbose_name="标准分值", null=False)
+
+    required = models.BooleanField(default=True, help_text="这个问题是否必须回答")
+
+    questionnaire = models.ForeignKey(Questionnaire)
+    order_in_list = models.IntegerField(default=1)  # 在问卷列表中的顺序，从1开始
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+
+    class Meta:
+        verbose_name = u"问题"
+        verbose_name_plural = verbose_name
+
+
+class AnswerSheet(models.Model):
+    judge = models.ForeignKey(UserProfile, verbose_name="评分人")
+    player = models.ForeignKey(UserProfile, verbose_name="受评人")
+    questionnaire = models.ForeignKey(Questionnaire, verbose_name="对应问卷")
+
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    modified_at = models.DateTimeField(auto_now=True, editable=False)
+    is_active = models.BooleanField(default=True)
+    total_score = models.IntegerField(max_length=100, verbose_name="总得分", null=False)
+
+    class Meta:
+        verbose_name = u"答卷"
+        verbose_name_plural = verbose_name
+
+
+class Answer(models.Model):
+    answer_sheet = models.ForeignKey(AnswerSheet)
+    question = models.ForeignKey(Question)
+    choices = models.IntegerField(max_length=100, verbose_name="得分", null=False)
+    text = models.TextField(verbose_name="备注")
+
+    class Meta:
+        verbose_name = u"答案"
+        verbose_name_plural = verbose_name
--- apps/users/models.py
+++ apps/users/models.py
@@ -1,4 +1,45 @@
 from django.contrib.auth.models import AbstractUser
-from django.db import models
 
 # Create your models here.
+from django.db import models
+from scores.models import Questionnaire
+
+
+class UserProfile(AbstractUser):
+    nick_name = models.CharField(max_length=50, verbose_name=u"昵称", default="")
+    birthday = models.DateField(verbose_name=u"生日", null=True, blank=True)
+    gender = models.CharField(max_length=6, choices=(("mule", u"男"), ("femule", u"女"), ("unknow", u"保密")), default="unknow")
+    address = models.CharField(max_length=100, default=u"")
+    mobile = models.CharField(max_length=11, null=True, blank=True)
+    # position = models.ForeignKey(Position)
+    # section = models.ForeignKey(Section)
+    # superior = models.ForeignKey(UserProfile)
+
+    class Meta:
+        verbose_name = u"用户信息"
+        verbose_name_plural = verbose_name
+
+    def get_anonymous_name(self):
+        pass
+
+    def __unicode__(self):
+        return self.username
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=50, verbose_name=u"名称", null=False)
+    describe = models.TextField(verbose_name="职位描述")
+    level = models.IntegerField(max_length=3, verbose_name=u"级别", null=False)
+
+    class Meta:
+        verbose_name = u"职位"
+        verbose_name_plural = verbose_name
+
+
+class Section(models.Model):
+    name = models.CharField(max_length=50, verbose_name=u"名称", null=False)
+    describe = models.TextField(verbose_name="部门描述")
+
+    class Meta:
+        verbose_name = u"部门"
+        verbose_name_plural = verbose_name
